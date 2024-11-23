4498e04cff8cec2635efc7e6113b53a14fa9d5db
Aishah999 <aishaah@code.edu.az>
2024-11-23 14:15:08 +0400

    add commit

---
+++ Task5/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task5;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public interface IAccount
+    {
+        bool PasswordChecker(string password);
+        void ShowInfo();
+    }
+    public class User: IAccount
+    {
+        public int Id
+        {
+            get;private set;
+        }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        private string _password;
+        private static int _idCounter = 1;
+    }
+    public User (string fullname,string email,string password)
+    {
+        
+        fullname = fullname;
+        email = email;
+
+        if(PasswordChecker(password))
+        {
+            _password = password;
+        }
+        else
+        {
+            ArgumentException("Sifre teleblere cavab vermir");
+        }
+                
+    }
+}
+
